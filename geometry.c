79918370603c18ee6b1788a0f32029841e35e95a
Purnima Surve <surve.purnima@gmail.com>
2019-03-30 21:35:44 +0530

    Create geometry.c

---
+++ geometry.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include<math.h>
+int main(void) {
+	// your code goes here
+int l=7,b=5,r=7;
+double circ;
+int peri=2*(l+b);
+int area=l*b;
+printf("Perimeter=%d\n Area=%d\n",peri,area);
+	circ=3.14*pow(r,2);
+	printf("Area=%f",circ);
+	return 0;
+}
+
