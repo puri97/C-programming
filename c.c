2c3b2441e159d26318eb292ab141328894ad6f16
Purnima Surve <surve.purnima@gmail.com>
2019-03-30 21:22:45 +0530

    Create c.c

---
+++ c.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void) {
+	// your code goes here
+	printf("  ######\n");
+	printf("##      ##\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("#\n");
+	printf("##      ##\n");
+	printf("  ######\n");
+
+	return 0;
+}
+
